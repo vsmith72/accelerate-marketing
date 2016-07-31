b29c3476a456a2d3496eecba1820139445a15293
Vanessa Smith <vsmith72@gmail.com>
2016-07-30 17:05:13 -0700

    First Commit

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="site-content">
+  <div id="content" role="main">
+
+    <?php while ( have_posts() ) : the_post();
+    $services = get_field('services');
+    $image_1  = get_field('image_1');
+    $size     = 'full';
+    ?>
+    <article class="case-study">
+      <aside class="left case-study-sidebar">
+        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+        <h5><?php echo $services; ?></h5>
+        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>" class="read-more-link">View Project <span>&rsaquo;</span></a>
+      </aside>
+      <div class="right case-study-images">
+       <a href="<?php the_permalink() ?>">
+        <?php if ($image_1) { echo wp_get_attachment_image( $image_1, $size); } ?>
+        </a>
+      </div>
+     </article>
+    <?php endwhile; // end of the loop. ?>
+
+  </div><!-- #content -->
+</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/accelerate-theme/functions.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Accelerate Marketing functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ *
+ * @link http://codex.wordpress.org/Theme_Development
+ * @link http://codex.wordpress.org/Child_Themes
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * @link http://codex.wordpress.org/Plugin_API
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+/**
+ * Register menus.
+ */
+function accelerate_setup() {
+  register_nav_menus( array(
+    'top-nav' => __( 'Top Nav', 'accelerate' ),
+    'social-media'  => __( 'Social Media Nav', 'accelerate' ),
+  ) );
+}
+add_action( 'after_setup_theme', 'accelerate_setup' );
+
+/**
+ * Register widget area.
+ */
+function accelerate_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Widget Area', 'accelerate' ),
+		'id'            => 'sidebar-1',
+		'description'   => __( 'Add widgets here to appear in your blog sidebar.', 'accelerate' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'accelerate_widgets_init' );
+
+// defines custom markup for post comments
+function accelerate_comments($comment, $args, $depth) {
+	$comment  = '<li class="comment">';
+	$comment .=	'<header class="comment-head">';
+	$comment .= '<span class="comment-author">' . get_comment_author() . '</span>';
+	$comment .= '<span class="comment-meta">' . get_comment_date('m/d/Y') . '&emsp;|&emsp;' . get_comment_reply_link(array('depth' => $depth, 'max_depth' => 5)) . '</span>';
+	$comment .= '</header>';
+	$comment .= '<div class="comment-body">';
+	$comment .= '<p>' . get_comment_text() . '</p>';
+	$comment .= '</div>';
+	$comment .= '</li>';
+
+	echo $comment;
+}
