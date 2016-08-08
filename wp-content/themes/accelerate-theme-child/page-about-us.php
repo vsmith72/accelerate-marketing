5708c154067746d13d397b420e918f7fe529f607
Vanessa Smith <vsmith72@gmail.com>
2016-08-07 19:56:01 -0700

    added font awesome

--- wp-content/themes/accelerate-theme-child/page-about-us.php
+++ wp-content/themes/accelerate-theme-child/page-about-us.php
@@ -15,13 +15,68 @@
 get_header(); ?>
 <section class="about-page">
   <div class="site-content flex">
-    <?php while ( have_posts() ) : the_post(); ?>
+    <?php while ( have_posts() ) : the_post();
+      //set up variables
+      $services = get_field('services');
+      $summary = get_field('summary');
+      $service_1 = get_field('service_1');
+      $service_2 = get_field('service_2');
+      $service_3 = get_field('service_3');
+      $service_4 = get_field('service_4');
+      $service_image_1 = get_field('service_image_1');
+      $service_image_2 = get_field('service_image_2');
+      $service_image_3 = get_field('service_image_3');
+      $service_image_4 = get_field('service_image_4');
+      $service_summary_1 = get_field('service_summary_1');
+      $service_summary_2 = get_field('service_summary_2');
+      $service_summary_3 = get_field('service_summary_3');
+      $service_summary_4 = get_field('service_summary_4');
+      $size = "full";
+    ?>
+
     <div class="about-hero about">
       <?php the_content(); ?>
     </div>
     <?php endwhile; // end of the loop. ?>
   </div><!-- .container -->
 </section><!-- .home-page -->
-
+<section class="about-content">
+  <h4><?php echo $services ?></h4>
+  <p class="center"><?php echo $summary ?></p>
+  <div class="service-1 clearfix">
+    <figure><?php echo wp_get_attachment_image( $service_image_1, $size); ?></figure>
+    <div class="service-feed">
+      <h2><?php echo $service_1 ?></h2>
+      <p><?php echo $service_summary_1 ?></p>
+    </div>
+  </div>
+  <div class="service-2 clearfix">
+    <figure><?php echo wp_get_attachment_image( $service_image_2, $size); ?></figure>
+    <div class="service-feed">
+      <h2><?php echo $service_2 ?></h2>
+      <p><?php echo $service_summary_2 ?></p>
+    </div>
+  </div>
+  <div class="service-3 clearfix">
+    <figure><?php echo wp_get_attachment_image( $service_image_3, $size); ?></figure>
+    <div class="service-feed">
+      <h2><?php echo $service_3 ?></h2>
+      <p><?php echo $service_summary_3 ?></p>
+    </div>
+  </div>
+  <div class="service-4 clearfix">
+    <figure><?php echo wp_get_attachment_image( $service_image_4, $size); ?></figure>
+    <div class="service-feed">
+      <h2><?php echo $service_4 ?></h2>
+      <p><?php echo $service_summary_4 ?></p>
+    </div>
+  </div>
+</section>
+<section>
+  <div class="about-cta">
+    <span>Intersted in working with us?</span>
+    <button class="about-btn">Contact Us</button>
+  </div>
+</section>
 
 <?php get_footer(); ?>
